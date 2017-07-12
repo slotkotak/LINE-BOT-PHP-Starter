e65537489817c8bc09bc907d2d856d0dc3757594
slotkotak <slotkotak@gmail.com>
2017-07-12 16:13:19 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'g3vcpG2D34EUbk80CC2DW5XwWjhn3KNEAeWCDEyfuG4f1AZceazZgE0CUY/29EM+wFfRNRSiXYH7wn4HAm47EgP8l+SlNqaGM7ucP06jBdVica3ckXD6gtM+AfzssbPmvGKVDoCQ/hB57Qpks11DVgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
