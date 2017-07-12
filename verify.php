4bfbc766b91cd0a819b71bd8f254b5ea5ce7bd30
slotkotak <slotkotak@gmail.com>
2017-07-12 16:35:27 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,9 @@
 <?php
-$access_token = 'U3466e0a63988a02413aca3a97c9d37dc';
+$access_token = 'g3vcpG2D34EUbk80CC2DW5XwWjhn3KNEAeWCDEyfuG4f1AZceazZgE0CUY/29EM+wFfRNRSiXYH7wn4HAm47EgP8l+SlNqaGM7ucP06jBdVica3ckXD6gtM+AfzssbPmvGKVDoCQ/hB57Qpks11DVgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
-
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
